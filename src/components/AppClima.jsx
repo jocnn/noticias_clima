4b46fc2a4463af16d2d72e1d5d81f74f2a29eb0a
jocnn <ijann_@outlook.com>
2022-05-18 10:58:53 -0300

    mostrando el resultado y agragando estilos

--- src/components/AppClima.jsx
+++ src/components/AppClima.jsx
@@ -1,10 +1,21 @@
+import useClima from "../hooks/useClima"
+
 import Formulario from "./Formulario"
+import Resultado from "./Resultado"
 
 const AppClima = () => {
+
+  const { resultado } = useClima()
+
   return (
     <>
       <main className="dos-columnas">
         <Formulario />
+
+        {
+          resultado?.name ? 
+          <Resultado /> : <p>El clima se va a mostrar aquí</p>
+        }
       </main>
 
     </>
---
+++ src/components/Resultado.jsx
@@ -0,0 +1,26 @@
+import useClima from "../hooks/useClima"
+
+const Resultado = () => {
+
+  const { resultado } = useClima()
+  console.info(resultado)
+  
+  const { name, main } = resultado
+
+  return (
+    <div className="contenedor clima">
+      <h2>
+        El Clima de {name} es: 
+      </h2>
+
+      <p>{main.temp} <span>&#x2103;</span></p>
+
+      <div className="temp_min_max">
+        <p>Mín: {main.temp_min} <span>&#x2103;</span></p>
+        <p>Máx: {main.temp_max} <span>&#x2103;</span></p>
+      </div>
+    </div>
+  )
+}
+
+export default Resultado
