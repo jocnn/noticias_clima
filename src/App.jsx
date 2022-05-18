b6f58e50744d55da732b5e24e5acd23c4bbb1205
jocnn <ijann_@outlook.com>
2022-05-18 08:18:11 -0300

    creacion del provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import AppClima from "./components/AppClima"
+import { ClimaProvider } from "./context/ClimaProvider"
 
 function App() {
 
   return (
-    <div>
+    <ClimaProvider>
+      <header>
+        <h1>Buscador de Clima</h1>
+      </header>
       <AppClima />
-    </div>
+    </ClimaProvider>
   )
 }
 
